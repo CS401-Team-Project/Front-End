beba798ceb0d67dab512dc2e74efd379795bdb83
Andrei Cozma <andreig992@gmail.com>
2022-02-25 17:56:02 -0500

    Added simple landing page

---
+++ src/routes/LandingRoutes.js
@@ -0,0 +1,33 @@
+import { lazy } from 'react';
+
+// project imports
+import Loadable from 'ui-component/Loadable';
+import MinimalLayout from 'layout/MinimalLayout';
+
+const LandingPage = Loadable(lazy(() => import('views/landing-page/LandingPage')));
+// login option 3 routing
+const AuthLogin3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Login3')));
+const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
+
+// ==============================|| AUTHENTICATION ROUTING ||============================== //
+
+const AuthenticationRoutes = {
+    path: '/',
+    element: <MinimalLayout />,
+    children: [
+        {
+            path: '/',
+            element: <LandingPage />
+        },
+        {
+            path: '/login',
+            element: <AuthLogin3 />
+        },
+        {
+            path: '/register',
+            element: <AuthRegister3 />
+        }
+    ]
+};
+
+export default AuthenticationRoutes;
