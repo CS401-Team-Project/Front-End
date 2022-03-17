0a2521e1849f4aa951ef2b4326a1c724d0d0bc1d
Andrei Cozma <me@andreicozma.com>
2022-03-16 22:25:02 -0400

    Quick update/fix

--- src/views/pages/groups.js
+++ src/views/pages/groups.js
@@ -1,20 +1,20 @@
 // project imports
-import MainCard from "ui-component/cards/MainCard";
-import SecondaryAction from "../../ui-component/cards/CardSecondaryAction";
+import { Grid } from "@mui/material";
+import { gridSpacing } from "../../store/constant";
+import Summary from "../../ui-component/cards/Summary";
+import MainContent from "../../ui-component/cards/MainContent";
 // ==============================|| TYPOGRAPHY ||============================== //
 
 const Groups = () => {
     return (
-        <MainCard title="My Groups" secondary={<SecondaryAction link="https://next.material-ui.com/system/typography/" />}>
-            <Grid container spacing={gridSpacing}>
-                <Grid item>
-                    <Summary></Summary>
-                </Grid>
-                <Grid item>
-                    <MainContent title="Joined Groups"></MainContent>
-                </Grid>
+        <Grid container spacing={gridSpacing}>
+            <Grid item>
+                <Summary />
             </Grid>
-        </MainCard>
+            <Grid item>
+                <MainContent title="Joined Groups" />
+            </Grid>
+        </Grid>
     );
 };
 
