893220c4c8093d722c2b7a067fa7fd2bdc3709e8
Jacob <jacobleonard545@gmail.com>
2022-04-02 15:30:13 -0400

    revert earning card

--- src/views/pages/dashboard/components/EarningCard.js
+++ src/views/pages/dashboard/components/EarningCard.js
@@ -144,7 +144,7 @@ const EarningCard = ({ isLoading }) => {
                                 <Grid container alignItems="center">
                                     <Grid item>
                                         <Typography sx={{ fontSize: "2.125rem", fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>
-                                            $152.20
+                                            $500.00
                                         </Typography>
                                     </Grid>
                                     <Grid item>
@@ -169,7 +169,7 @@ const EarningCard = ({ isLoading }) => {
                                         color: theme.palette.secondary[200]
                                     }}
                                 >
-                                    You Owe This Overall
+                                    Total Earning
                                 </Typography>
                             </Grid>
                         </Grid>
