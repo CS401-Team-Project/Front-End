8943a7abc3474b40cbad8c31e27d7a8960637f81
Jacob <jacobleonard545@gmail.com>
2022-04-02 12:15:13 -0400

    update

--- src/views/pages/group/ItemData.js
+++ src/views/pages/group/ItemData.js
@@ -5,19 +5,19 @@ const ItemData = [
             {
                 name: "Milk",
                 quantity: 1,
-                unit_price: 3.00,
+                unit_price: "3.00",
                 owed_by: "Jacob Leonard",
                 description: "Description!"
             },
             {
                 name: "Spaghetti",
                 quantity: 6,
-                unit_price: 7.00,
+                unit_price: "7.00",
                 owed_by: "Dian Martin",
-                description: "Description 2!" 
+                description: "Description 2!"
             }
         ]
     }
-]
+];
 
 export default ItemData;
