e3c9ee20b6d4251c7e1734a79e194bc7965de620
Andrei Cozma <me@andreicozma.com>
2022-04-03 16:04:28 -0400

    Removed unneeded routes and centered logo

--- src/routes/DefaultRoutes.js
+++ src/routes/DefaultRoutes.js
@@ -1,14 +1,8 @@
-import { lazy } from "react";
-
-// project imports
+import { lazy } from "react"; // project imports
 import Loadable from "ui-component/Loadable";
 import MinimalLayout from "../layout/MinimalLayout";
 
 const LandingPage = Loadable(lazy(() => import("views/pages/landing")));
-// login option 3 routing
-const AuthLogin3 = Loadable(lazy(() => import("views/pages/authentication/authentication3/Login3")));
-const AuthRegister3 = Loadable(lazy(() => import("views/pages/authentication/authentication3/Register3")));
-const ApiTest = Loadable(lazy(() => import("views/pages/api-test")));
 
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
@@ -19,18 +13,6 @@ const DefaultRoutes = {
         {
             path: "/",
             element: <LandingPage />
-        },
-        {
-            path: "/login",
-            element: <AuthLogin3 />
-        },
-        {
-            path: "/register",
-            element: <AuthRegister3 />
-        },
-        {
-            path: "/api-test",
-            element: <ApiTest />
         }
     ]
 };
