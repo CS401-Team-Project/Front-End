c5a35aaa48d55c41aec105820471ae9c358de140
Andrei Cozma <me@andreicozma.com>
2022-04-02 21:21:53 -0400

    Quick update/fix

--- src/api/transaction.js
+++ src/api/transaction.js
@@ -5,62 +5,58 @@ import client from "./client";
 // TODO: Add transaction API calls here ( similar to post request in test.js )
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#transactioninfo-1 */
-const getTransaction = (token, id) =>
+const getTransaction = (token, transaction_id) =>
     client.post("/transaction/info", {
         token: token,
-        id: id
+        id: transaction_id
     });
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#transactioncreate-1 */
-const createTransaction = (token, id, title, description, vendor, date) =>
+const createTransaction = (token, group_id, title, description, vendor, date) =>
     client.post("/transaction/create", {
         token: token,
-        id: id,
+        id: group_id,
         description: description,
         vendor: vendor,
         date: date
     });
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#transactionupdate-1 */
-const updateTransaction = (token, id, data) =>
+const updateTransaction = (token, transaction_id, data) =>
     client.post("/transaction/update", {
         token: token,
-        id: id,
+        id: transaction_id,
         data: data
     });
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#transactiondelete-1 */
-const deleteTransaction = (token, id) =>
+const deleteTransaction = (token, transaction_id) =>
     client.post("/transaction/delete", {
         token: token,
-        id: id
+        id: transaction_id
     });
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#transactionadd-item-1 */
-const addItem = (token, id, name, quantity, unit_price, owed_by, description) =>
+const addItem = (token, transaction_id, items_list) =>
     client.post("/transaction/add-item", {
         token: token,
-        id: id,
-        name: name,
-        quantity: quantity,
-        unit_price: unit_price,
-        owed_by: owed_by,
-        description: description
+        id: transaction_id,
+        items: items_list
     });
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#transactionremove-item-1 */
-const removeItem = (token, id, data) =>
+const removeItem = (token, transaction_id, data) =>
     client.post("/transaction/remove-item", {
         token: token,
-        id: id,
+        id: transaction_id,
         data: data
     });
 
 /* Doc: https://github.com/CS401-Team-Project/Back-End/blob/main/API-DOCS/TRANSACTION.md#iteminfo-1 */
-const getItem = (token, id) =>
+const getItem = (token, item_id) =>
     client.post("/item/info", {
         token: token,
-        id: id
+        id: item_id
     });
 
 export default {
